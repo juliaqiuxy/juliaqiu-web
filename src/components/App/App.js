1b31ece9ce4bc51c3a6c997b5930019eb9295746
Julia Qiu <juliaqiuxy@gmail.com>
2017-11-06 15:01:39 -0800

    remove unused code

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,51 +1,9 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Home from '../Home/Home';
-import Portfolio from '../Portfolio/Portfolio';
-import FlippableCard from '../FlippableCard/FlippableCard';
 import './App.css';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.renderFrontCard = this.renderFrontCard.bind(this);
-    this.handleFlipCard = this.handleFlipCard.bind(this);
-
-    this.state = {
-      currentCard: undefined,
-    };
-  }
-
-  handleFlipCard(currentCard) {
-    console.log(currentCard)
-    this.setState({
-      currentCard,
-    });
-  }
-
-  renderFrontCard() {
-    return (
-      <Home
-        onChangeCard={this.handleFlipCard}
-      />
-    );
-  }
-
-  render() {
-    return (
-      <FlippableCard
-        duriation={1}
-        cubicBezier={[0.15, 0.90, 0.25, 1.25]}
-        currentCard={this.state.currentCard}
-        renderFrontCard={this.renderFrontCard}
-      >
-        <Portfolio
-          id="portfolio"
-          onChangeCard={this.handleFlipCard}
-        />
-      </FlippableCard>
-    );
-  }
-}
+const App = () => (
+  <Home />
+);
 
 export default App;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,10 +2,9 @@ import React from 'react';
 import Logo from '../Logo/Logo';
 import Footer from '../Footer/Footer';
 import Bubbles from '../Bubbles/Bubbles';
-import flipButton from './flipButton.svg';
 import './Home.css';
 
-const Home = ({ onChangeCard }) => {
+const Home = () => {
   return (
     <div
       className="Home-container"
@@ -18,20 +17,3 @@ const Home = ({ onChangeCard }) => {
 };
 
 export default Home;
-// const HomeFlipButton = () => (
-//   <div className="FlipButton-container">
-//     <div className="row">
-//       <a
-//         className="col-1 offset-11"
-//         onClick={() => onChangeCard('portfolio')}
-//       >
-//         <div className="FlipButton-IconContainer">
-//           <img alt="flip button" src={flipButton} />
-//         </div>
-//       </a>
-//     </div>
-//   </div>
-// );
-// <HomeFlipButton onChangeCard={onChangeCard} />
-
-// onClick={() => onChangeCard('portfolio')}
