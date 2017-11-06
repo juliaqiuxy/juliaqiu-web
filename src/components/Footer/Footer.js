73e1739f9732249ca82ced2eeece17b0e865f4b7
Julia Qiu <juliaqiuxy@gmail.com>
2017-11-06 14:53:34 -0800

    upgrade font awesome and add portfolio link

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -8,11 +8,30 @@ const stopEventPropagation = (event) => {
 const Footer = () => (
   <div className="Footer-container">
     <div className="row align-items-center" >
-      <div className="col-2 offset-3 col-md-1 offset-md-9">
+      <div className="col-2 offset-2 col-md-1 offset-md-8">
         <div className="Footer-icon-container">
           <a
             onClick={stopEventPropagation}
-            href={'https://mail.google.com/mail/?view=cm&fs=1&to=juliaqiuxy@gmail.com'}
+            href={'http://portfolio.juliaqiu.com'}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <span key="portfolio" className="fa-stack Footer-icon">
+              <i
+                className="fa fa-circle fa-stack-2x"
+              />
+              <i
+                className="fa fa-user-circle fa-stack-1x fa-inverse"
+              />
+            </span>
+          </a>
+        </div>
+      </div>
+      <div className="col-2 col-md-1">
+        <div className="Footer-icon-container">
+          <a
+            onClick={stopEventPropagation}
+            href={'https://mail.google.com/mail/?view=cm&fs=1&to=julia@juliaqiu.com'}
             target="_blank"
             rel="noopener noreferrer"
           >
