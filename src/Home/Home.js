95dda8b9b86842d812b812baf12589bf2b947f8c
Julia Qiu <juliaqiuxy@gmail.com>
2018-01-09 13:32:16 -0800

    automate holiday logo

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -4,15 +4,14 @@ import OpenSource from '../OpenSource/OpenSource';
 import Footer from '../Footer/Footer';
 import Article from '../Articles/Articles';
 import Contact from '../Contact/Contact';
-
-import logo from '../images/logo-holiday.svg';
+import SmartLogo from './SmartLogo';
 
 import './Home.css';
 
 const Home = () => (
   <div className="Home-container">
     <div className="Home-logo-container">
-      <img src={logo} alt="Julia" />
+      <SmartLogo />
     </div>
     <div className="Home-content">
       <Article />
