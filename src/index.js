59aed878569dc7778df6e32c80d2cd4d9ac6d8dd
Julia Qiu <juliaqiuxy@gmail.com>
2018-05-02 18:14:46 -0700

    disable service worker

--- src/index.js
+++ src/index.js
@@ -5,7 +5,5 @@ import 'normalize.css';
 import './index.css';
 
 import App from './App/App';
-import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('🦄'));
-registerServiceWorker();
