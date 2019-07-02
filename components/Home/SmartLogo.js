7bd45993ce876a2bd4e251ea5793dba15dd23460
Julia Qiu <juliaqiuxy@gmail.com>
2019-07-01 22:00:16 -0700

    add pride theme to smart logo

--- components/Home/SmartLogo.js
+++ components/Home/SmartLogo.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
+const NOVEMBER = 10;
+
 const isHolidays = () => {
   const now = new Date();
   const currentMonth = now.getMonth();
   let holidayStart;
   let holidayEnd;
 
-  if (currentMonth < 10) {
+  if (currentMonth < NOVEMBER) {
     holidayStart = new Date(now.getFullYear() - 1, 10, 15);
     holidayEnd = new Date(now.getFullYear(), 0, 5);
   } else {
@@ -21,10 +23,28 @@ const isHolidays = () => {
   return false;
 };
 
+const isPride = () => {
+  const now = new Date();
+  const prideStart = new Date(now.getFullYear(), 5, 15);
+  const prideEnd = new Date(now.getFullYear(), 6, 2);
+
+  if (prideStart < now && now < prideEnd) {
+    return true;
+  }
+
+  return false;
+};
+
 const SmartLogo = () => {
   if (isHolidays()) {
     return (
       <img src="/static/images/logo-holiday.svg" alt="Julia" />
+    );
+  }
+
+  if (isPride()) {
+    return (
+      <img src="/static/images/logo-pride.svg" alt="Julia" />
     );
   }
 
