925e2e48b24c4e97f0e5b93ed23451ab7cae97d4
Julia Qiu <julia@julia.dev>
2020-11-14 09:37:44 -0800

    add talk link

--- src/components/Talks/Talks.js
+++ src/components/Talks/Talks.js
@@ -13,14 +13,14 @@ const Talks = () => (
     <Category category="Talks" />
     <Talk
       src="/videos/zeit-day.mp4"
-      link="https://vercel.com/tv"
+      link="http://bit.ly/zeitday-juliaqiu"
       linkText="vercel.com/tv"
       title="Migrating Slope Ninja to Zeit for Fun and Profit"
       name="Zeit Day 2018"
     />
     <Talk
       src="/videos/nextjs-conf.mp4"
-      link="https://nextjs.org/conf/speakers/juliaqiuxy"
+      link="https://bit.ly/2UtdR4E"
       linkText="nextjs.org/conf"
       title="CSS + Cookies: A Perfectionist's Approach to Theming in the World of Server-side Rendered Apps"
       name="Next.js Conf"
