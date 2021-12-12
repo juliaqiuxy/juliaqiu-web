f96d60ed8fddae01b38784f0015cc17e62421cce
Julia Qiu <julia@julia.dev>
2021-12-12 14:47:25 -0800

    reverse talk order

--- src/components/Talks/Talks.js
+++ src/components/Talks/Talks.js
@@ -12,18 +12,18 @@ const Talks = () => (
   <Container>
     <Category category="Talks" />
     <Talk
+      src="/videos/nextjs-conf.mp4"
+      link="https://bit.ly/2UtdR4E"
+      linkText="nextjs.org/conf"
+      title="CSS + Cookies: A Perfectionist's Approach to Theming in the World of Server-side Rendered Apps"
+      name="Next.js Conf 2020"
+    />
+    <Talk
       src="/videos/zeit-day.mp4"
       link="http://bit.ly/zeitday-juliaqiu"
       linkText="vercel.com/tv"
       title="Migrating Slope Ninja to Zeit for Fun and Profit"
       name="Zeit Day 2018"
-    />
-    <Talk
-      src="/videos/nextjs-conf.mp4"
-      link="https://bit.ly/2UtdR4E"
-      linkText="nextjs.org/conf"
-      title="CSS + Cookies: A Perfectionist's Approach to Theming in the World of Server-side Rendered Apps"
-      name="Next.js Conf"
     />
   </Container>
 );
