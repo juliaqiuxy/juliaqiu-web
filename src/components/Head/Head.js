a4a61b2bb71e5688efa8f66a00ddb7ac3e9a1de5
Julia Qiu <julia@julia.dev>
2021-12-12 09:47:21 -0800

    fix font warning

--- src/components/Head/Head.js
+++ src/components/Head/Head.js
@@ -106,7 +106,7 @@ const Head = withRouter((props) => (
     <link rel="apple-touch-icon" href="https://julia.dev/images/favicon.png" />
 
     <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;300;500&display=swap" rel="stylesheet" />
 
     {/* OPEN GRAPH */}
